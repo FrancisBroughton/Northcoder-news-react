d091754ddf10ca254e214901d91dda2daaa4cf81
Jenny Redfern <jchunyi.ho@googlemail.com>
2017-11-23 14:38:20 +0000

    implement reducer and reducer tests for articles

---
+++ northcoder-news-react/src/reducer/articles.js
@@ -0,0 +1,37 @@
+import * as types from '../types'
+
+export const initialState = {
+    loading: false,
+    error: null,
+    payload: []
+  };
+  
+export default (prevState = initialState, action) => {
+    switch (action.type) { 
+
+    case types.FETCH_ARTICLES_REQUEST:
+      return Object.assign({}, prevState, {
+        loading: !prevState.loading,
+        error: null,
+        payload: []
+      });
+
+      case types.FETCH_ARTICLES_SUCCESS:
+      return Object.assign({}, prevState, {
+        loading: !prevState.loading,
+        error: null,
+        payload: action.payload
+      });
+
+      case types.FETCH_ARTICLES_FAILURE:
+      return Object.assign({}, prevState, {
+        loading: false,
+        error: action.payload,
+        data: []
+      });
+  
+    default:
+      return prevState;
+
+    }
+};    
---
+++ northcoder-news-react/src/spec/Articles.reducer.test.js
@@ -0,0 +1,50 @@
+import { expect } from 'chai';
+import articlesReducer from '../reducer';
+import {initialState} from '../reducer/articles';
+import * as types from '../types'
+
+import {
+    fetchArticlesRequest,
+    fetchArticlesSuccess,
+    fetchArticlesFailure
+} from '../actions/articles';
+
+const prevState = initialState || prevState;
+
+describe('articles reducer', () => {
+    describe('default behaviour', () => {
+        it('returns the passed prevState if an unrecognised action is passed', () => {
+            const action = {type:"whatever"};
+            const newState = articlesReducer(prevState, action);
+            expect(newState.articles).to.equal(prevState);
+        });
+        it('uses the initial state if no prevState is passed', () => {
+            const action = {type: "fetchArticleRequest"};
+            const newState = articlesReducer(undefined, action);
+            expect(newState.articles).to.equal(initialState);
+        });
+        it('handles fetchArticlesRequest', () => {
+            const action = fetchArticlesRequest();
+            const newState = articlesReducer(prevState, action);
+            expect(newState.articles.loading).to.eql(true);
+            expect(newState.articles.error).to.eql(null);
+            expect(newState.articles.payload).to.eql([]);
+        });
+        it('handles fetchArticlesSuccess', () => {
+            const data = "hello";
+            const action = fetchArticlesSuccess(data);
+            const newState = articlesReducer(prevState, action);
+            expect(newState.articles.payload).to.be.equal("hello");
+            expect(newState.articles.error).to.be.equal(null);
+            expect(newState.articles.loading).to.be.equal(true);
+        });
+        it('should return the correct action for fetchArticlesFailure', () => {
+            const error = 'error';
+            const action = fetchArticlesFailure(error);
+            const newState = articlesReducer(prevState, action);
+            expect(newState.articles.error).to.be.equal('error')
+            expect(newState.articles.loading).to.be.equal(false)
+            expect(newState.articles.payload).to.be.eql([])
+        });
+    })
+});    

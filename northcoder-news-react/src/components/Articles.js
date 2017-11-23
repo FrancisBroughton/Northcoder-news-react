910a6cd940f0d09866ee803f62b0acf233c7be26
Jenny Redfern <jchunyi.ho@googlemail.com>
2017-11-23 16:13:23 +0000

    implement function to fetch all articles in Articles Page

--- northcoder-news-react/src/components/Articles.js
+++ northcoder-news-react/src/components/Articles.js
@@ -1,17 +1,60 @@
 import React, { Component } from 'react';
-import {
-  BrowserRouter,
-  Route,
-  Link,
-  Switch
-  } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { fetchAllArticles } from '../actions/articles'
 
 class Articles extends Component {
-  render() {
-    return (
-              Articles
-            );
-          }
+  constructor(props) {
+    super(props)
 }
+
+componentDidMount() {
+    this.props.fetchAllArticles()
+}
+
+render() {
+    return (
+        <div className="body">
+
+        <div className="title">
+        <ul>
+            <li><i className="fa fa-heart"></i></li> 
+            <li><h1>All articles</h1></li>
+            <li><i className="fa fa-heart"></i></li> 
+        </ul>    
+        </div>
+
+            {this.props.articles.map((article, i) => {
+                    return (
+                        <div key={i} className="article">
+                            <ul>
+                                <li> <span>Article ID:</span> {article._id}</li>
+                                <li> <span>Article Title:</span> {article.title}</li>
+                                <li> <span>Created By: </span>{article.created_by}</li> <br />
+                                <li> {article.body}</li>
+                            </ul>
+                        </div>
+                    )
+                }
+            )}
+        </div>
+    );
+}
+}
+
+function mapStateToProps(state) {
+return {
+    loading: state.articles.loading,
+    error: state.articles.error,
+    articles: state.articles.payload
+};
+}
+function mapDispatchToProps(dispatch) {
+return {
+    fetchAllArticles: (data) => {
+        dispatch(fetchAllArticles());
+    }
+};
+}
+
             
-export default Articles;
+export default connect(mapStateToProps,mapDispatchToProps)(Articles);

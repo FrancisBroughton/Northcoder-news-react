f1580d78e134c5a1c1a70eb14372e85be5c97471
Jenny Redfern <jchunyi.ho@googlemail.com>
2017-11-26 17:19:39 +0000

    implement action test for fetch comments by article id

--- northcoder-news-react/src/spec/Articles.actions.test.js
+++ northcoder-news-react/src/spec/Articles.actions.test.js
@@ -1,26 +1,41 @@
 
-import {expect} from 'chai';
+import { expect } from 'chai';
 import * as articlesActions from '../actions/articles';
 import * as types from '../types';
 
-describe('Action creators', () => {
-        it('should return the correct action for fetchArticlesRequest', () => {
-            expect(articlesActions.fetchArticlesRequest()).to.eql({
-                type: types.FETCH_ARTICLES_REQUEST
-            });
+describe('Actions for Articles', () => {
+    it('should return the correct action for fetchArticlesRequest', () => {
+        expect(articlesActions.fetchArticlesRequest()).to.eql({
+            type: types.FETCH_ARTICLES_REQUEST
         });
-        it('should return the correct action for fetchArticlesSuccess', () => {
-            const data = [1,2,3,4];
-            expect(articlesActions.fetchArticlesSuccess(data)).to.eql({
-                type: types.FETCH_ARTICLES_SUCCESS,
-                payload: data
-            });
+    });
+    it('should return the correct action for fetchArticlesSuccess', () => {
+        const data = [1, 2, 3, 4];
+        expect(articlesActions.fetchArticlesSuccess(data)).to.eql({
+            type: types.FETCH_ARTICLES_SUCCESS,
+            payload: data
         });
-        it('should return the correct action for fetchArticlesFailure', () => {
-            const error = 'error';
-            expect(articlesActions.fetchArticlesFailure(error)).to.eql({
-                type: types.FETCH_ARTICLES_FAILURE,
-                payload: error
-            });
+    });
+    it('should return the correct action for fetchArticlesFailure', () => {
+        const error = 'error';
+        expect(articlesActions.fetchArticlesFailure(error)).to.eql({
+            type: types.FETCH_ARTICLES_FAILURE,
+            payload: error
         });
+    });
+    it('should return the correct action for fetchCommentsByArtIdSuccess', () => {
+        const data = [1, 2, 3, 4];
+        expect(articlesActions.fetchCommentsByArtIdSuccess(data)).to.eql({
+            type: types.FETCH_COMMENTS_BY_ART_ID_SUCCESS,
+            comments: data
+        });
+    });
+    it('should return the correct action for fetchCommentsByArtIdFailure', () => {
+        const error = 'error';
+        expect(articlesActions.fetchCommentsByArtIdFailure(error)).to.eql({
+            type: types.FETCH_COMMENTS_BY_ART_ID_FAILURE,
+            commentsError: error
+        });
+
+    });
 });

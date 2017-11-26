f06a2834e1bd35cc4906d8ec6c9d7c3f33fff324
Jenny Redfern <jchunyi.ho@googlemail.com>
2017-11-26 17:20:43 +0000

    implement action function for fetch comments by articles id

--- northcoder-news-react/src/actions/articles.js
+++ northcoder-news-react/src/actions/articles.js
@@ -21,6 +21,20 @@ export function fetchArticlesFailure(error) {
     };
 }
 
+export function fetchCommentsByArtIdSuccess(data) {
+    return {
+        type: types.FETCH_COMMENTS_BY_ART_ID_SUCCESS,
+        comments: data
+    };
+}
+
+export function fetchCommentsByArtIdFailure(error) {
+    return {
+        type: types.FETCH_COMMENTS_BY_ART_ID_FAILURE,
+        commentsError: error
+    };
+}
+
 export function fetchAllArticles(data){
     return function (dispatch){
         axios.get('https://northcoders-news-api.herokuapp.com/api/articles')
@@ -45,3 +59,16 @@ export function fetchArticlesByArticlesId(id){
     }
 }
 
+export function fetchCommentsByArticlesId(id){
+    console.log("in fetchCommentsByArticlesId")
+    return function (dispatch){
+        axios.get(`https://northcoders-news-api.herokuapp.com/api/articles/${id}/comments`)
+        .then(res => {
+            console.log(res.data)
+            dispatch(fetchCommentsByArtIdSuccess(res.data.comments))
+        })
+        .catch (err => {
+            dispatch(fetchCommentsByArtIdFailure(err))
+        })
+    }
+}

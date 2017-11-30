5aa6519b616bdd257bad500b865f9b25abd4ef3e
Jenny Redfern <jchunyi.ho@googlemail.com>
2017-11-30 16:46:09 +0000

    remove topic action tests from articles

--- northcoder-news-react/src/spec/Articles.actions.test.js
+++ northcoder-news-react/src/spec/Articles.actions.test.js
@@ -1,7 +1,6 @@
 
 import { expect } from 'chai';
 import * as articlesActions from '../actions/articles';
-import * as topicsActions from '../actions/topics';
 import * as types from '../types';
 
 describe('Actions for Articles', () => {
@@ -54,24 +53,4 @@ describe('Actions for Articles', () => {
     });
 });   
 
-describe('Actions for topics', () => { 
-    it('should return the correct action for fetchtopicsRequest', () => {
-        expect(topicsActions.fetchTopicsRequest()).to.eql({
-            type: types.FETCH_TOPICS_REQUEST
-        });
-    });
-    it('should return the correct action for fetchTopicsSuccess', () => {
-        const data = [1, 2, 3, 4];
-        expect(topicsActions.fetchTopicsSuccess(data)).to.eql({
-            type: types.FETCH_TOPICS_SUCCESS,
-            payload : data
-        });
-    });
-    it('should return the correct action for fetchTopicsFailure', () => {
-        const error = 'error';
-        expect(topicsActions.fetchTopicsFailure(error)).to.eql({
-            type: types.FETCH_TOPICS_FAILURE,
-            error: error
-        });    
-    });
-});    
+    

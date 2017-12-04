e6aaabc9c66f10e9e3c67e46217fc4aa959c112e
Jenny Redfern <jchunyi.ho@googlemail.com>
2017-12-04 10:39:17 +0000

    implement function to fetch all topics

--- northcoder-news-react/src/components/Topics.js
+++ northcoder-news-react/src/components/Topics.js
@@ -1,17 +1,51 @@
 import React, { Component } from 'react';
-import {
-  BrowserRouter,
-  Route,
-  Link,
-  Switch
-  } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import {fetchAllTopics} from '../actions/topics';
 
 class Topics extends Component {
-  render() {
-    return (
-      Topics
-    );
+
+  componentDidMount() {
+    this.props.fetchAllTopics()
   }
+
+  render() {
+    console.log(this.props)
+    return (
+      <div className="body container">
+      <div className="col-md-12">
+        <h1> Northcoders News </h1>
+        <div className="title">
+        <h2>Topics</h2>
+      </div>
+      </div>
+
+        {this.props.topics.map((topic, i) => {
+          return (
+            <div key={i} className="article">
+              <ul>
+                <li> <Link to={`/Topics`}>{topic.title}</Link></li>
+                </ul>
+            </div>
+          )
+        })}
+  </div>
+)};
 }
 
-export default Topics;
+function mapStateToProps(state) {
+  return {
+    loading: state.topics.loading,
+    error: state.topics.error,
+    topics: state.topics.payload
+  };
+}
+function mapDispatchToProps(dispatch) {
+  return {
+fetchAllTopics: (data) => {
+  dispatch(fetchAllTopics());
+}
+      }
+  };
+
+export default connect (mapStateToProps, mapDispatchToProps)(Topics);

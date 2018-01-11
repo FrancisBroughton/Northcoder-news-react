8a69e49246ef03e392387ae61ef4e5496ff5bbc7
Jenny Redfern <jchunyi.ho@googlemail.com>
2018-01-11 15:40:16 +0000

    implement comments to reducer

--- northcoder-news-react/src/reducer/index.js
+++ northcoder-news-react/src/reducer/index.js
@@ -1,11 +1,13 @@
 import {combineReducers} from 'redux';
 import articles from './articles';
 import topics from './topics';
+import comments from './comments';
 
 const reducer = combineReducers({
   articles: articles,
-  topics: topics
-  
+  topics: topics,
+  comments: comments
+
 });
 
 export default reducer;

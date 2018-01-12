7397dac12963bc9b756d05faa8297c539bf5b0ce
Jenny Redfern <jchunyi.ho@googlemail.com>
2018-01-12 13:23:14 +0000

    spec for add/ delete comments

---
+++ northcoder-news-react/src/spec/Comments.reducer.test.js
@@ -0,0 +1,69 @@
+import { expect } from 'chai';
+import commentsReducer from '../reducer';
+import {initialState} from '../reducer/comments';
+import * as types from '../types'
+
+import {
+    addComments,
+    addCommentsSuccess,
+    addCommentsFailure,
+    deleteComments,
+    deleteCommentsSuccess,
+    deleteCommentsFailure
+} from '../actions/articles';
+
+const prevState = initialState || prevState;
+
+describe('comments reducer', () => {
+    describe('default behaviour', () => {
+        it('returns the passed prevState if an unrecognised action is passed', () => {
+            const action = {type:"whatever"};
+            const newState = commentsReducer(prevState, action);
+            console.log(newState)
+            expect(newState.comments).to.equal(prevState);
+        });
+        it('uses the initial state if no prevState is passed', () => {
+            const action = {type: "addCommentRequest"};
+            const newState = commentsReducer(undefined, action);
+            expect(newState.comments).to.equal(initialState);
+        });
+        it('handles addCommentsSuccess', () => {
+            const data = {
+                comment: "added comment", 
+                username: "j-redfern"
+            }
+            const action = addCommentsSuccess(data);
+            const newState = commentsReducer(prevState, action);
+            expect(newState.comments.commentsPayload).to.be.equal(data);
+            expect(newState.comments.commentsError).to.be.equal(null);
+            expect(newState.comments.loading).to.be.equal(true);
+        });
+        it('handles addCommentsFailure', () => {
+            const error = 'error';
+            const action = addCommentsFailure(error);
+            const newState = commentsReducer(prevState, action);
+            expect(newState.comments.commentsError).to.be.equal('error')
+            expect(newState.comments.loading).to.be.equal(false)
+            expect(newState.comments.commentsPayload).to.be.eql([])
+        });
+        it('handles deleteCommentsSuccess', () => {
+            const data = {
+                comment: "delete this comment", 
+                username: "j-redfern"
+            }
+            const action = deleteCommentsSuccess();
+            const newState = commentsReducer(prevState, action);
+            expect(newState.comments.commentsPayload).to.be.equal(undefined);
+            expect(newState.comments.commentsError).to.be.equal(null);
+            expect(newState.comments.loading).to.be.equal(true);
+        });
+        it('handles deleteCommentsFailure', () => {
+            const error = 'error';
+            const action = deleteCommentsFailure(error);
+            const newState = commentsReducer(prevState, action);
+            expect(newState.comments.commentsError).to.be.equal('error')
+            expect(newState.comments.loading).to.be.equal(false)
+            expect(newState.comments.commentsPayload).to.be.eql([])
+        });
+    })
+}); 

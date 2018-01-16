1119b6e6763dcb84c68063d642ed8616d677e266
Jenny Redfern <jchunyi.ho@googlemail.com>
2018-01-16 17:09:03 +0000

    implement users in reducer

--- northcoder-news-react/src/reducer/index.js
+++ northcoder-news-react/src/reducer/index.js
@@ -2,11 +2,13 @@ import {combineReducers} from 'redux';
 import articles from './articles';
 import topics from './topics';
 import comments from './comments';
+import users from './users';
 
 const reducer = combineReducers({
   articles: articles,
   topics: topics,
-  comments: comments
+  comments: comments,
+  users: users
 
 });
 
